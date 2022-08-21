2ca9c5d4a1d236843cd47c36c3e8b6a495dddabc
mzeballosn <mzeballosn@gmail.com>
2022-08-21 19:41:58 -0400

    [add] new firebase config

--- src/containers/Success/index.jsx
+++ src/containers/Success/index.jsx
@@ -1,6 +1,5 @@
 import React, {useContext} from 'react';
 import AppContext from '../../context/AppContext';
-import MapViewLeaflet from '../../components/MapViewLeaflet';
 import './style.css';
 
 
@@ -14,7 +13,6 @@ function Success() {
         <h2>{`${buyer.name} , gracias por tu compra`}</h2>
         <span>Tu pedido llegara en 3 dias</span>
         <div className="Success-map">
-          <MapViewLeaflet />
         </div>
       </div>  
     </div>
